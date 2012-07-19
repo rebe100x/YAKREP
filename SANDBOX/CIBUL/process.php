1712aa1d42fea503ed028291f1504567b4bb3f73
Delezenne Florian <del.florian@gmail.com>
2012-07-19 19:18:16 +0200

    fixed api call, preview of event object

--- SANDBOX/CIBUL/process.php
+++ SANDBOX/CIBUL/process.php
@@ -2,33 +2,39 @@
 
 if (isset($_POST['cibulUrl'])){
 	$path = parse_url($_POST['cibulUrl'], PHP_URL_PATH);
-
 	$pathExploded = explode("/", $path);
 
 	$url = "https://api.cibul.net/v1/events/uid/";
 
-	$fields = array(
-		'event-slug'=>urlencode($pathExploded[2]),
-		'key'=>'22640375947e8efe580bbe056e4c7b60'
-	);
-
-	$fields_string = '';
-	foreach ($fields as $key=>$value) {
-		$fields_string .= $key.'='.$value.'&';
-	}
-
-	$fields_string = rtrim($fields_string,'&');
-
 	$ch = curl_init();
 
-	curl_setopt($ch,CURLOPT_URL,$url);
-	curl_setopt($ch,CURLOPT_POST,count($fi    elds));
-	curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_URL, $url . urlencode($pathExploded[2]) . '?key=22640375947e8efe580bbe056e4c7b60');
 
-	$result = curl_exec($ch);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	$json = trim(curl_exec($ch));
 	curl_close($ch);
-	echo $result;
+
+	$result = json_decode($json);
+
+	$uid = $result->data->uid;
+
+	$chuid = curl_init();
+
+	$url = 'https://api.cibul.net/v1/events/';
+
+	curl_setopt($chuid, CURLOPT_URL, $url . $uid . '?key=22640375947e8efe580bbe056e4c7b60');
+
+	curl_setopt($chuid, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($chuid, CURLOPT_SSL_VERIFYPEER, false);
+
+	$json = trim(curl_exec($chuid));
+	curl_close($chuid);
+
+	$result = json_decode($json);
+	var_dump($result);
+	
 }else{
 	$value = "";
 }

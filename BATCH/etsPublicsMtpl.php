394961073c169ff35bd86cfede28049a3e36ce56
Joseph El Khoury <joseph.elkhoury1@gmail.com>
2012-09-27 13:55:12 +0200

    removed unused lines from my batch

--- BATCH/etsPublicsMtpl.php
+++ BATCH/etsPublicsMtpl.php
@@ -54,95 +54,77 @@ if (($handle = fopen($filenameInput, "r")) !== FALSE)
 
 			if (stristr($data[9], "#LOISIR#SPORT#PATINOIRE")) 
 			{
-				//echo "media : $data[1] <br/>";
 				$currentObject->setCatPatinoire();
 			}
 			elseif (stristr($data[9], "#LOISIR#CULTURE#PLANETARIUM")) 
 			{
-				//echo "Planétarium : $data[1] <br/>";
 				$currentObject->setCatPlanetarium();
 				$currentObject->yakTag["enfants"] = 1;
 			}
 			elseif (stristr($data[9], "#EDUCATION#CRECHE")) 
 			{
-				//echo "Aquarium : $data[1] <br/>";
 				$currentObject->setCatCreche();
 				$currentObject->yakTag["enfants"] = 1;
 			}
 			elseif (stristr($data[9], "#EDUCATION#ECOLE#PRIMAIRE")) 
 			{
-				//echo "Aquarium : $data[1] <br/>";
 				$currentObject->setCatPrimaire();
 				$currentObject->yakTag["enfants"] = 1;
 			}
 			else if (stristr($data[9], "#EDUCATION#MAISONDEQUARTIER")) 
 			{
-				//echo "media : $data[1] <br/>";
 				$currentObject->setCatMaisonDeQuartier();
 			}
 			else if (stristr($data[9], "#CULTURE#THEATRE")) 
 			{
-				//echo "media : $data[1] <br/>";
 				$currentObject->setCatTheatre();
 			}
 			else if (stristr($data[9], "#CULTURE#EXPOSITION"))
 			{
-				//echo "media : $data[1] <br/>";
 				$currentObject->setCatExposition();
 			}
 			else if (stristr($data[9], "#CULTURE#MUSEE"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatMusee();
 			}
 			else if (stristr($data[9], "#CULTURE#CONCERT"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatConcert();
 			}
 			else if (stristr($data[9], "#CULTURE#MEDIATHEQUE"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatMediatheque();
 			}
 			else if (stristr($data[9], "#SPORT#VOLLEY"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatVolley();
 			}
 			else if (stristr($data[9], "#SPORT#PISCINE"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatPiscine();
 			}
 			else if (stristr($data[9], "#SPORT#GYMNASE"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatGymnase();
 			}
 			else if (stristr($data[9], "#SPORT#FOOTBALL"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatFootball();
 			}
 			else if (stristr($data[9], "#SPORT#RUBGBY"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatRugby();
 			}
 			else if (stristr($data[9], "#SPORT#TENNIS"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatTennis();
 			}
 			else if (stristr($data[9], "#SPORT"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatSport();
 			}
 			else if (stristr($data[9], "#SPORT#PETANQUE"))
 			{
-				//echo "autres (musée) : $data[1] <br/>";
 				$currentObject->setCatPetanque();
 			}
 			/*print "<pre>";
